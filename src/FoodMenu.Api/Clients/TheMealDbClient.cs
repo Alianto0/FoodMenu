abcecd9712a5911aed383fc93b905a613db6166e
Tadas G <gedminast@gmail.com>
2024-04-03 09:40:48 +0300

    implement suggestions by category and area

--- src/FoodMenu.Api/Clients/TheMealDbClient.cs
+++ src/FoodMenu.Api/Clients/TheMealDbClient.cs
@@ -5,22 +5,48 @@ namespace FoodMenu.Api.Clients
 {
     public interface ITheMealDbClient
     {
+        Task<MealsDbResponse> FilterMealByArea(string area);
+        Task<MealsDbResponse> FilterMealByCategory(string category);
         Task<MealsDbResponse> SearchMealByName(string name);
     }
 
     public class TheMealDbClient : ITheMealDbClient
     {
+        private const string baseUrl = "www.themealdb.com/api/json/v1/1";
         private RestClient _client;
         public TheMealDbClient()
         {
             _client = new RestClient();
         }
 
+
         public async Task<MealsDbResponse> SearchMealByName(string name)
         {
-            var uriBuilder = new UriBuilder("www.themealdb.com/api/json/v1/1/search.php");
+            var uriBuilder = new UriBuilder($"{baseUrl}/search.php");
             uriBuilder.Query = $"?s={name}";
 
+
+            var restRequest = new RestRequest(uriBuilder.Uri.ToString());
+            var response = await _client.GetAsync<MealsDbResponse>(restRequest);
+            return response;
+        }
+
+
+
+        public async Task<MealsDbResponse> FilterMealByCategory(string category)
+        {
+            var uriBuilder = new UriBuilder($"{baseUrl}/filter.php");
+            uriBuilder.Query = $"?c={category}";
+
+            var restRequest = new RestRequest(uriBuilder.Uri.ToString());
+            var response = await _client.GetAsync<MealsDbResponse>(restRequest);
+            return response;
+        }
+
+        public async Task<MealsDbResponse> FilterMealByArea(string area)
+        {
+            var uriBuilder = new UriBuilder($"{baseUrl}/filter.php");
+            uriBuilder.Query = $"?a={area}";
 
             var restRequest = new RestRequest(uriBuilder.Uri.ToString());
             var response = await _client.GetAsync<MealsDbResponse>(restRequest);
--- src/FoodMenu.Api/Logic/MealsRetriever.cs
+++ src/FoodMenu.Api/Logic/MealsRetriever.cs
@@ -6,35 +6,57 @@ namespace FoodMenu.Api.Logic
 {
     public interface IMealsRetriever
     {
-        Task<List<Meal>> GetMealByName(string mealName);
+        Task<Meal> GetMealByName(string mealName);
     }
 
     public class MealsRetriever : IMealsRetriever
     {
         private readonly ITheMealDbClient mealDbClient;
+        private const int SuggestedMealsByCategory = 5;
+        private const int SuggestedMealsByArea = 3;
 
         public MealsRetriever(ITheMealDbClient mealDbClient)
         {
             this.mealDbClient = mealDbClient;
         }
 
-        public async Task<List<Meal>> GetMealByName(string mealName)
+        public async Task<Meal> GetMealByName(string mealName)
         {
             var response = await mealDbClient.SearchMealByName(mealName);
-
-            List<Meal> responseMeals;
 
             if (response.Meals == null || response.Meals.Count == 0)
             {
                 throw new MealNotFoundException($"Meal with the name of '{mealName}' was not found");
             }
-            else
-            {
-                responseMeals = response.Meals.Select(meal => new Meal { Area = meal.strArea, Category = meal.strCategory, Name = meal.strMeal }).ToList();
-              
-            }
 
-            return responseMeals;
+            var   responseMeal = response.Meals.Select(meal => new Meal { Area = meal.strArea, Category = meal.strCategory, Name = meal.strMeal }).First();
+
+            responseMeal.SuggestionsByCategory = await GetMealSuggestionsByCategory(responseMeal.Category);
+            responseMeal.SuggestionsByArea = await GetMealSuggestionsByArea(responseMeal.Area);
+
+            return responseMeal;
+        }
+
+        private async Task<List<SuggestionMeal>> GetMealSuggestionsByCategory(string category)
+        {
+            var response = await mealDbClient.FilterMealByCategory(category);
+
+            List<SuggestionMeal> suggestionMeals;
+
+            suggestionMeals = response.Meals.Select(meal => new SuggestionMeal { Name = meal.strMeal }).Take(SuggestedMealsByCategory).ToList();            
+
+            return suggestionMeals;
+        }
+
+        private async Task<List<SuggestionMeal>> GetMealSuggestionsByArea(string area)
+        {
+            var response = await mealDbClient.FilterMealByArea(area);
+
+            List<SuggestionMeal> suggestionMeals;
+
+            suggestionMeals = response.Meals.Select(meal => new SuggestionMeal { Name = meal.strMeal }).Take(SuggestedMealsByArea).ToList();
+
+            return suggestionMeals;
         }
     }
 }
