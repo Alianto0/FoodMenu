92df3068bb9d138adac6a615eb544ac9f4b4ce65
Tadas G <gedminast@gmail.com>
2024-04-03 10:13:39 +0300

    add controller tests, improve swagger

--- src/FoodMenu.Api/Controllers/MealsController.cs
+++ src/FoodMenu.Api/Controllers/MealsController.cs
@@ -18,7 +18,14 @@ namespace FoodMenu.Api.Controllers
             this.mealsRetriever = mealsRetriever;
         }
 
+        /// <summary>
+        /// Gets meal by name.
+        /// </summary>
+        /// <param name="name">Name of the meal to dsearch for.</param>
+        /// <returns>Meal with suggested alternatives by category and area.</returns>       
         [HttpGet("{name}")]
+        [ProducesResponseType(typeof(Meal), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetMealByName(string name)
         {
             try
@@ -27,10 +34,8 @@ namespace FoodMenu.Api.Controllers
             }
             catch (MealNotFoundException ex)
             {
-                return StatusCode(StatusCodes.Status404NotFound, new ProblemDetails() { Title = "Not found", Status = StatusCodes.Status404NotFound, Detail = ex.Message });
-                
-            }         
-
+                return StatusCode(StatusCodes.Status404NotFound, new ProblemDetails() { Title = "Not found", Status = StatusCodes.Status404NotFound, Detail = ex.Message });                
+            }
         }
     }
 }
--- tests/FoodMenu.Api.UnitTests/Controllers/MealsControllerTests.cs
+++ tests/FoodMenu.Api.UnitTests/Controllers/MealsControllerTests.cs
@@ -1,6 +1,12 @@
-﻿using FoodMenu.Api.Controllers;
+﻿using FluentAssertions;
+using FoodMenu.Api.Controllers;
+using FoodMenu.Api.Exceptions;
 using FoodMenu.Api.Logic;
+using FoodMenu.Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Moq;
 using Objectivity.AutoFixture.XUnit2.AutoMoq.Attributes;
 
 namespace FoodMenu.Api.UnitTests.Controllers
@@ -9,14 +15,41 @@ namespace FoodMenu.Api.UnitTests.Controllers
     {
         [Theory]
         [AutoMockData]
-        public void GetMealByName_HappyPath_DoesNotThrow(ILogger<MealsController> logger, IMealsRetriever mealsRetriever)
+        public async void GetMealByName_HappyPath_DoesNotThrow(ILogger<MealsController> logger, IMealsRetriever mealsRetriever)
         {
+            const string testMealName = "Test Meal";
             var mealsController = new MealsController(logger, mealsRetriever);
-            var testMealName = "Test Meal";
 
-            var response = mealsController.GetMealByName(testMealName);
+            // Act
+            var response = await mealsController.GetMealByName(testMealName);
+            var statusCodeResult = response as OkObjectResult;
 
-            Assert.NotNull(response);
+            // Assert
+            statusCodeResult.Should().NotBeNull();
+            statusCodeResult.StatusCode.Should().Be(StatusCodes.Status200OK);
+            statusCodeResult.Value.Should().NotBeNull();
+            statusCodeResult.Value.Should().BeOfType<Meal>();
+        }
+
+        [Theory]
+        [AutoMockData]
+        public async void GetMealByName_MealsRetrieverThrowingMealNotFoundException_Returns404(ILogger<MealsController> logger, Mock<IMealsRetriever> mealsRetriever)
+        {
+            // Arrange
+            const string testMealName = "Test Meal";
+            mealsRetriever.Setup(mealsRetriever => mealsRetriever.GetMealByName(It.IsAny<string>())).ThrowsAsync(new MealNotFoundException(testMealName));
+
+            var mealsController = new MealsController(logger, mealsRetriever.Object);
+
+            // Act
+            var response = await mealsController.GetMealByName(testMealName);
+            var statusCodeResult = response as ObjectResult;
+
+            // Assert
+            statusCodeResult.Should().NotBeNull();
+            statusCodeResult.StatusCode.Should().Be(StatusCodes.Status404NotFound);
+            statusCodeResult.Value.Should().NotBeNull();
+            statusCodeResult.Value.Should().BeOfType<ProblemDetails>();
         }
     }
 }
